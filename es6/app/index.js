02029f02e182ed096c8604677dc26de83300a291
Kedanco <kelvinchowjunjie120@hotmail.com>
2018-03-26 12:14:11 +0800

    Finished the course

--- es6/app/index.js
+++ es6/app/index.js
@@ -1,128 +1,41 @@
-// let keyword & variable scope
-  let a = 'hello';
 
-  console.log(a);
-// scope can be used to make the variable private from public access
-  {
-    let salary = 90000;
-  }
-    console.log(salary);
+// JSX
+// Adds XML-like syntax to javascript.
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-// const keyword
-  const g = 2;
-// push works!
-  const array = [1,2,3];
-  array.push(4);
-  console.log(array);
+const api_key = '7a97b7e3f55903f4d9fbbb947ea4dda3';
 
-// this won't work!
-  array = [7,8,9];
-
-// Template literals (using backticks)
-  var a = 'hello';
-  var b = 'world';
-  console.log(`hello ${b}`);
-
-
-//Spread operator - Unlimited amount of arguments!
-  console.log(a);
-  function print(...z){
-    console.log(a,b,c);
-  }
-  print(1,2,3,4,5,6);
-
-// Destructuring Assignment - Array
-  let c = [100,200,300,400];
-  let [a, ...b] = c;
-  console.log(a,b);
-
-  let fellowship = ["Frodo", "Gandalf", "Aragon"];
-  let [hobbit, wizard, ranger] = fellowship;
-  console.log(hobbit, wizard, ranger);
-
-
-// Destructuring Assignment - Objects
-// matches variable name w/ property
-// let wizard = {magical: true, power: 10};
-// let {magical, power} = wizard;
-
-  let magical = true;
-  let power = 2;
-
-  let ranger = {magical: false, power: 9};
-// parenthesis makes sure reassignment through destructuring is possible
-
-  ({magical, power} = ranger);
-  console.log(magical, power);
-
-// Arrow functions are anonymous
-// No names identifier
-
-  // function blastoff(){
-  //   console.log("3..2..1.. blast off!");
-  // }
-
-  // setTimeout(() => {
-  //   console.log("3..2..1.. blast off!");
-  // },1000);
-
-  const blastoff = () => {
-    console.log("3..2..1.. blast off!"); 
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      city: "Jakarta",
+      description: ''
+    }
   }
 
-  blastoff();
-
-// Arrow functions do not bind their own `this`
-
-  this.a = 25;
-
-  let print = function(){
-    this.a = 50; //Different this.a from global
-    console.log('this.a', this.a);
-  }  
-
-  let arrowPrint = () => {
-    // arrow's this will follow the global scope's this.
-    console.log('this.a in arrowPrint', this.a);
+  componentDidMount(){
+    this.grabWeather(this.state.city);
   }
 
-  print();
+  grabWeather(city){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({description: json.weather[0].description})
+    });
+  }
 
-// The map function
+  render(){
+    return (
+      <div>
+        <h1>Weather Report for {this.state.city}!</h1>
+        <h2>{this.state.description}</h2>
+      </div>
+    )
+  }
+}
 
-  let points = [10,20,30];
-
-  // let addOne = function(element){
-  //   return element + 1;
-  // }
-
-  points = points.map(element => element + 1);
-  // when no braces, the function auto returns, thus no need return statement
-
-  console.log(points);
-
-// Filter
-
-  // let isPassing = (grade) => {
-  //   return grade >= 70;
-  // }
-
-  let scores = [90,85,67,71,84,60,39,52];
-
-  let passing = scores.filter(element => element >= 70);
-
-  console.log(passing);
-
-// Other helper methods include
-// -find
-// -forEach
-// -reduce, some, keys, values
-
-
-// importing from other files
-import {students, total} from './students';
-console.log(students, total);
-
-import multiply from './calculator';
-console.log(multiple(3,5));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ es6/app/notes.js
@@ -0,0 +1,161 @@
+// // let keyword & variable scope
+//   let a = 'hello';
+
+//   console.log(a);
+// // scope can be used to make the variable private from public access
+//   {
+//     let salary = 90000;
+//   }
+//     console.log(salary);
+
+
+// // const keyword
+//   const g = 2;
+// // push works!
+//   const array = [1,2,3];
+//   array.push(4);
+//   console.log(array);
+
+// // this won't work!
+//   array = [7,8,9];
+
+// // Template literals (using backticks)
+//   var a = 'hello';
+//   var b = 'world';
+//   console.log(`hello ${b}`);
+
+
+// //Spread operator - Unlimited amount of arguments!
+//   console.log(a);
+//   function print(...z){
+//     console.log(a,b,c);
+//   }
+//   print(1,2,3,4,5,6);
+
+// // Destructuring Assignment - Array
+//   let c = [100,200,300,400];
+//   let [a, ...b] = c;
+//   console.log(a,b);
+
+//   let fellowship = ["Frodo", "Gandalf", "Aragon"];
+//   let [hobbit, wizard, ranger] = fellowship;
+//   console.log(hobbit, wizard, ranger);
+
+
+// // Destructuring Assignment - Objects
+// // matches variable name w/ property
+// // let wizard = {magical: true, power: 10};
+// // let {magical, power} = wizard;
+
+//   let magical = true;
+//   let power = 2;
+
+//   let ranger = {magical: false, power: 9};
+// // parenthesis makes sure reassignment through destructuring is possible
+
+//   ({magical, power} = ranger);
+//   console.log(magical, power);
+
+// // Arrow functions are anonymous
+// // No names identifier
+
+//   // function blastoff(){
+//   //   console.log("3..2..1.. blast off!");
+//   // }
+
+//   // setTimeout(() => {
+//   //   console.log("3..2..1.. blast off!");
+//   // },1000);
+
+//   const blastoff = () => {
+//     console.log("3..2..1.. blast off!"); 
+//   }
+
+//   blastoff();
+
+// // Arrow functions do not bind their own `this`
+
+//   this.a = 25;
+
+//   let print = function(){
+//     this.a = 50; //Different this.a from global
+//     console.log('this.a', this.a);
+//   }  
+
+//   let arrowPrint = () => {
+//     // arrow's this will follow the global scope's this.
+//     console.log('this.a in arrowPrint', this.a);
+//   }
+
+//   print();
+
+// // The map function
+
+//   let points = [10,20,30];
+
+//   // let addOne = function(element){
+//   //   return element + 1;
+//   // }
+
+//   points = points.map(element => element + 1);
+//   // when no braces, the function auto returns, thus no need return statement
+
+//   console.log(points);
+
+// // Filter
+
+//   // let isPassing = (grade) => {
+//   //   return grade >= 70;
+//   // }
+
+//   let scores = [90,85,67,71,84,60,39,52];
+
+//   let passing = scores.filter(element => element >= 70);
+
+//   console.log(passing);
+
+// // Other helper methods include
+// // -find
+// // -forEach
+// // -reduce, some, keys, values
+
+
+// // importing from other files
+//   import {students, total} from './students';
+//   console.log(students, total);
+
+//   import multiply from './calculator';
+//   console.log(multiple(3,5));
+
+// // OOP in ES6 - class inheritance.
+
+//   import Entity from './entity';
+
+//   class Hobbit extends Entity{
+//     constructor(name, height){
+//       super(name, height);
+//     }
+//     greet(){
+//       console.log(`Hello! I'm ${this.name} from the Shire!`);
+//     }
+//   }
+
+//   let Frodo = new Hobbit("Frodo Baggings",4.5);
+//   Frodo.greet();
+
+
+// // JSX
+// // Adds XML-like syntax to javascript.
+
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends Component{
+//   render(){
+//     return {
+//       <div>ReactJS and JSX in action!</div>
+//     }
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
